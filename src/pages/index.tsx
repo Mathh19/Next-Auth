8a6e7cec48b0049152b637f9e9e59ee93cad2af7
Matheus <mf635097@gmail.com>
2022-10-27 06:40:32 -0300

    Config environment variables

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,16 @@
-import Home from 'templates/Home';
+import { GetServerSideProps } from 'next';
 
 export default function Index() {
-  return <Home />;
+  return (
+    <h1>
+      <span>Hello World</span>
+    </h1>
+  );
 }
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  console.log(process.env.VAR);
+  return {
+    props: {},
+  };
+};
