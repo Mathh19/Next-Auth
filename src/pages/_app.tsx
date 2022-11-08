e73763473c480128130067a02b95701aa35fb1a7
Matheus <mf635097@gmail.com>
2022-11-08 08:37:53 -0300

    Adding feedback when routes change

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,7 @@ import { ThemeProvider } from 'styled-components';
 import { theme } from '../styles/theme';
 import { AppProps } from 'next/app';
 import { SessionProvider } from 'next-auth/react';
+import NextNProgress from 'nextjs-progressbar';
 
 import '../../public/assets/fonts/styles.css';
 import { GlobalStyles } from '../styles/global-styles';
@@ -10,6 +11,13 @@ function MyApp({ Component, pageProps: { ...pageProps } }: AppProps) {
   return (
     <SessionProvider>
       <ThemeProvider theme={theme}>
+        <NextNProgress
+          color={theme.colors.deepWhite}
+          startPosition={0.3}
+          stopDelayMs={200}
+          height={3}
+          showOnShallow={true}
+        />
         <Component {...pageProps} />
         <GlobalStyles />
       </ThemeProvider>
